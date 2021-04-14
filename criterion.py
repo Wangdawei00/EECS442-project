Minor fix and change the calculation of accuracy, divide by y_hat

criterion.py
@@ -10,7 +10,7 @@ class GradientLoss(nn.Module):
         self.convX = nn.Conv2d(1, 1, (3, 3), (1, 1), (1, 1), bias=False)
         self.convY = nn.Conv2d(1, 1, (3, 3), (1, 1), (1, 1), bias=False)
         x_gradient = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
-        y_gradient = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]])
+        y_gradient = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
         self.convX.weight = nn.Parameter(torch.from_numpy(x_gradient).float().unsqueeze(0).unsqueeze(0))
         self.convY.weight = nn.Parameter(torch.from_numpy(y_gradient).float().unsqueeze(0).unsqueeze(0))
         for conv in [self.convY, self.convX]:

util.py
@@ -3,11 +3,11 @@ import numpy as np
 import torch
 
 
-def make_plot(loss: list, accuracy: list):
+def make_plot(loss_list: list, acc: list):
     """
-    Make a side-by-side training plot of loss and accuracy.
+    Make a side-by-side training plot of loss_list and accuracy.
 
-    loss: List of the loss values
+    loss_list: List of the loss_list values
     accuracy: List of the accuracy values
     """
     fig, axes = plt.subplots(1, 2)
@@ -16,9 +16,9 @@ def make_plot(loss: list, accuracy: list):
     axes[1].set_xlabel('Epoch')
     axes[0].set_ylabel('Accuracy')
     axes[1].set_ylabel('Loss')
-    x = np.arange(len(loss))
-    axes[0].plot(x, accuracy, '--o')
-    axes[1].plot(x, loss, '--o')
+    x = np.arange(len(loss_list))
+    axes[0].plot(x, acc, '--o')
+    axes[1].plot(x, loss_list, '--o')
     plt.show()
 
 
@@ -37,10 +37,10 @@ def evaluate_model(model, loader, device):
             batch = batch.to(device)
             truth = truth.to(device)
             pred = model(batch)
-            accuracies.append(torch.mean(torch.abs(pred - truth)).item())
-        accuracy = sum(accuracies) / len(accuracies)
-        print("Evaluation accuracy: {}".format(accuracy))
-    return accuracy
+            accuracies.append(torch.mean(torch.abs(pred - truth) / truth).item())
+        acc = sum(accuracies) / len(accuracies)
+        print("Evaluation accuracy: {}".format(acc))
+    return acc
 
 
 if __name__ == '__main__':