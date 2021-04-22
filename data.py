1c69255877ba8a695f0159dc3a08d84302adfa2d
ruiyiw <ruiyiw@umich.edu>
2021-04-22 03:03:43 +0000

    Add normalization

--- data.py
+++ data.py
@@ -163,10 +163,13 @@ def getNoTransform(is_test=False):
     ])
 
 def getDefaultTrainTransform():
+    # Normalization
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     return transforms.Compose([
         RandomHorizontalFlip(),
         RandomChannelSwap(0.5),
-        ToTensor()
+        ToTensor(),
+        normalize
     ])
 
 def getTrainingValidationTestingData(data_size, path, batch_size):
