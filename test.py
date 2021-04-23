94322d0233834d3b5327844abaf768a28cb0c06e
ruiyiw <ruiyiw@umich.edu>
2021-04-23 15:30:39 +0000

    Fix model loading and csv path

--- test.py
+++ test.py
@@ -10,6 +10,12 @@ from sklearn import metrics
 from torch.nn.functional import softmax
 import argparse as arg
 
+from res50 import Res50
+from dense169 import Dense169
+from dense121 import Dense121
+from mob_v2 import Mob_v2
+
+
 torch.manual_seed(42)
 np.random.seed(42)
 random.seed(42)
@@ -23,21 +29,40 @@ def main(device=torch.device('cuda:0')):
     # Parsing
     args = parser.parse_args()
     # Data loaders
+    
+    # TODO:
+    ####### Enter the model selection here! #####
+    modelSelection = "mob_v2"
+    
     datasize = args.datasize
     filename = "nyu_new.zip"
     pathname = f"data/{filename}"
-    te_loader = getTestingData(datasize, pathname, batch_size=config("unet.batch_size"))
+    csv = "data/nyu_csv.zip"
+    te_loader = getTestingData(datasize, csv, pathname, batch_size=config(modelSelection+".batch_size"))
 
     # Model
-    model = Net()
+    if modelSelection.lower() == 'res50':
+        model = Res50()
+    elif modelSelection.lower() == 'dense121':
+        model = Dense121()
+    elif modelSelection.lower() == 'mob_v2':
+        model = Mob_v2()
+    elif modelSelection.lower() == 'dense169':
+        model = Dense169()
+    elif modelSelection.lower() == 'mob':
+        model = Net()
+    elif modelSelection.lower() == 'squeeze':
+        model = Squeeze()
+    else:
+        assert False, 'Wrong type of model selection string!'
     model = model.to(device)
 
     # define loss function
     # criterion = torch.nn.L1Loss()
 
     # Attempts to restore the latest checkpoint if exists
-    print("Loading unet...")
-    model, start_epoch, stats = utils.restore_checkpoint(model, utils.config("unet.checkpoint"))
+    print(f"Loading {mdoelSelection}...")
+    model, start_epoch, stats = utils.restore_checkpoint(model, utils.config(modelSelection+".checkpoint"))
     acc, loss = utils.evaluate_model(model, te_loader, device, test=True)
     # axes = util.make_training_plot()
     print(f'Test Error:{acc}')
